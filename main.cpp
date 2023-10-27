5699533c46c4b53d46d553ddd95168f05886fe6c
Badra <badra022@ubuntu.ubuntu-domain>
2023-10-28 00:10:06 +0200

    implemented the average feature

--- main.cpp
+++ main.cpp
@@ -7,13 +7,22 @@ int getSum(const std::vector<int>& vec) {
     for (int value : vec) {
         sum += value;
     }
+    
     return sum;
+}
+
+int getAvg(const std::vector<int>& vec){
+    int result = getSum(vec);
+    
+    return result / vec.size();
 }
 
 int main() {
     std::vector<int> myVector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sumResult = getSum(myVector);
+    int avgResult = getAvg(myVector);
     std::cout << "Sum of elements in the vector: " << sumResult << std::endl;
+    std::cout << "Average of elements in the vector: " <<  avgResult << std::endl;
 
     return 0;
 }
