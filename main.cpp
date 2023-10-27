80150c64cca6b1da8a53028697d5ebcd90cd97f4
Badra <badra022@ubuntu.ubuntu-domain>
2023-10-28 00:16:20 +0200

    implemented the minimum feature

--- main.cpp
+++ main.cpp
@@ -10,10 +10,24 @@ int getSum(const std::vector<int>& vec) {
     return sum;
 }
 
+
+int getMin(const std::vector<int>& vec){
+    int min = vec[0];
+    for(int value : vec) {
+        if(value < min) {
+            min = value;
+        }
+    }
+
+    return min;
+}
+
 int main() {
     std::vector<int> myVector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sumResult = getSum(myVector);
+    int minResult = getMin(myVector);
     std::cout << "Sum of elements in the vector: " << sumResult << std::endl;
+    std::cout << "Min of elements in the vector: " << minResult << std::endl;
 
     return 0;
 }
